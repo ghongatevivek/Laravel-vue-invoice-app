3934163533ec85ac7d51f8f55199892d903304e2
ghongatevivek <ghongatevivek1212@gmail.com>
2024-03-02 14:16:13 +0530

    Worked on DB, seed, factory setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(1)->create();
+        // \App\Models\User::factory(1)->create();
+        // \App\Models\Counter::factory(1)->create();
+        // \App\Models\Product::factory(4)->create();
+        // \App\Models\Customer::factory(5)->create();
+        // \App\Models\Invoice::factory(5)->create();
+        \App\Models\InvoiceItem::factory(5)->create();
     }
 }
